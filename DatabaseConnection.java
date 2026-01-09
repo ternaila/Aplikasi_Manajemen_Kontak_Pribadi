f40138fac490b0b8130494a143965a03ffb79c1c
terdian <2400018261@webmail.uad.ac.id>
2026-01-09 16:21:50 +0700

    Create DatabaseConnection.java

---
+++ DatabaseConnection.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ContactManager";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Koneksi Error: " + e.getMessage());
+        }
+        return conn;
+    }
+}
